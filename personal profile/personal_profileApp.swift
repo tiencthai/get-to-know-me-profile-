fe98c1b54245619f000f384446e6d7bda61b1949
Tien <tiencthai2006@gmaik.com>
2023-06-25 21:14:50 -0400

    Initial Commit

---
+++ personal profile/personal_profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_profileApp.swift
+//  personal profile
+//
+//  Created by Tien Thai on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct personal_profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ personal profileUITests/personal_profileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  personal_profileUITestsLaunchTests.swift
+//  personal profileUITests
+//
+//  Created by Tien Thai on 6/25/23.
+//
+
+import XCTest
+
+final class personal_profileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
